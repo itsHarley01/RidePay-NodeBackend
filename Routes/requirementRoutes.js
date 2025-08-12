99c0429ac3b8664454402b727e640daeca4801f4
itsHarley01 <harlae116@gmail.com>
2025-08-13 02:33:03 +0800

    rm getallreq func

--- Routes/requirementRoutes.js
+++ Routes/requirementRoutes.js
@@ -5,11 +5,9 @@ const {
   getRequirements,
   updateRequirement,
   deleteRequirement,
-  getAllRequirements
 } = require('../Controllers/requirementsController');
 
 router.post('/req/:category', createRequirement);
-router.post('/req', getAllRequirements);
 router.get('/req/:category', getRequirements);
 router.put('/req/:category/:id', updateRequirement);
 router.delete('/req/:category/:id', deleteRequirement);
