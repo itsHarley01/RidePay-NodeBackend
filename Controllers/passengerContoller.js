10ac3e6149c1b6ce66bbe6f76c1a1753ee04d939
itsHarley01 <harlae116@gmail.com>
2025-08-14 21:23:23 +0800

    s

--- Controllers/passengerContoller.js
+++ Controllers/passengerContoller.js
@@ -23,7 +23,7 @@ const registerPassenger = async (req, res) => {
     const currentDate = new Date().toISOString();
 
     // Save to Realtime Database
-    await set(ref(db,`p4zs3gr_usr_uu34/${uid}`),{
+    await db.ref(`p4zs3gr_usr_uu34/${uid}`).set({
       systemUid,
       firstName,
       lastName,
